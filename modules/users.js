25aa2dc0be4dec28e0793ffeb7c4c182ad73276f
BwanaRob <bwanabob@gmail.com>
2024-01-02 05:01:47 -0600

    bypass avatar fetch

--- modules/users.js
+++ modules/users.js
@@ -3,27 +3,30 @@ var knownAvatars = {};
 var defaultURL = 'https://www.redditstatic.com/avatars/defaults/v2/avatar_default_7.png';
 
 async function getAvatar(username) {
-    var returnURL = defaultURL;
-    var cached = false;
-    if (knownAvatars[username]) {
-      returnURL = knownAvatars[username];
-      cached = true;
-    } else {
-      try {
-        returnURL = await fetch(`https://api.reddit.com/user/${username}/about`)
-          .then(response => response.json())
-          .then(data => {
-            const returnAvatar = data.data.icon_img.replace(/&amp;/g, '&') || data.data.snoovatar_img.replace(/&amp;/g, '&') || "unknown";
-            if (returnAvatar !== "unknown") {
-              knownAvatars[username] = returnAvatar;
-            }
-            return returnAvatar;
-          })
-      } catch (err) {
-        console.error(err);
-      }
+  // until we figure out what changed in the API...
+  return { url: defaultURL, cached: true };
+
+  var returnURL = defaultURL;
+  var cached = false;
+  if (knownAvatars[username]) {
+    returnURL = knownAvatars[username];
+    cached = true;
+  } else {
+    try {
+      returnURL = await fetch(`https://api.reddit.com/user/${username}/about`)
+        .then(response => response.json())
+        .then(data => {
+          const returnAvatar = data.data.icon_img.replace(/&amp;/g, '&') || data.data.snoovatar_img.replace(/&amp;/g, '&') || "unknown";
+          if (returnAvatar !== "unknown") {
+            knownAvatars[username] = returnAvatar;
+          }
+          return returnAvatar;
+        })
+    } catch (err) {
+      console.error(err);
     }
-    return { url: returnURL, cached: cached };
   }
-  
-  module.exports = {getAvatar};
+  return { url: returnURL, cached: cached };
+}
+
+module.exports = { getAvatar };
